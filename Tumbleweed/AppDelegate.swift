946c302de88265f7a61ec3a66f1573e7e960619f
Chloe Stars <hachidorii@icloud.com>
2015-07-17 19:51:46 -0700

    Initial Commit

---
+++ Tumbleweed/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Tumbleweed
+//
+//  Created by Chloe Stars on 7/17/15.
+//  Copyright © 2015 Chloe Stars. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Tumbleweed/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Tumbleweed
+//
+//  Created by Chloe Stars on 7/17/15.
+//  Copyright © 2015 Chloe Stars. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

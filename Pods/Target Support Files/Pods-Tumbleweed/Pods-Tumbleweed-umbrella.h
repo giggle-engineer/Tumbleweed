b16b7c954530d12eb6b281cc631debb12ac3c0c8
Chloe Stars <hachidorii@icloud.com>
2015-07-19 21:04:04 -0700

    Checking the Pods. Hopefully this allows it to build?

---
+++ Pods/Target Support Files/Pods-Tumbleweed/Pods-Tumbleweed-umbrella.h
@@ -0,0 +1,6 @@
+#import <Cocoa/Cocoa.h>
+
+
+FOUNDATION_EXPORT double Pods_TumbleweedVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_TumbleweedVersionString[];
+
---
+++ Pods/Target Support Files/TMTumblrSDK/TMTumblrSDK-umbrella.h
@@ -0,0 +1,11 @@
+#import <Cocoa/Cocoa.h>
+
+#import "TMAPIClient.h"
+#import "TMOAuth.h"
+#import "TMTumblrAuthenticator.h"
+#import "TMSDKFunctions.h"
+#import "TMSDKUserAgent.h"
+
+FOUNDATION_EXPORT double TMTumblrSDKVersionNumber;
+FOUNDATION_EXPORT const unsigned char TMTumblrSDKVersionString[];
+

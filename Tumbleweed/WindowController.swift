38e294b3d8b720d44afaf4a60f3f4e62b8e7b4ce
Chloe Stars <hachidorii@icloud.com>
2015-07-26 13:23:15 -0700

    Move window titlebar styling to the appropriate function.

--- Tumbleweed/WindowController.swift
+++ Tumbleweed/WindowController.swift
@@ -10,18 +10,19 @@ import Cocoa
 import INAppStoreWindow
 
 class WindowController : NSWindowController {
-    @IBOutlet var appWindow : INAppStoreWindow!
     @IBOutlet var titleBarView : NSView!
     override func windowWillLoad() {
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "handleReopen", name: "handleReopen", object: nil)
+    }
+    
+    override func awakeFromNib() {
+        let window = self.window as! INAppStoreWindow
+        window.trafficLightButtonsLeftMargin = 14.0
+        window.titleBarHeight = 33.0
         
-        //        let appWindow = self.window as! INAppStoreWindow
-        appWindow.trafficLightButtonsLeftMargin = 14.0
-        appWindow.titleBarHeight = 33.0
-        
-        self.titleBarView.frame = appWindow.titleBarView.bounds
+        self.titleBarView.frame = window.titleBarView.bounds
         self.titleBarView.autoresizingMask  = [NSAutoresizingMaskOptions.ViewWidthSizable, NSAutoresizingMaskOptions.ViewHeightSizable]
-        appWindow.titleBarView.addSubview(self.titleBarView)
+        window.titleBarView.addSubview(self.titleBarView)
     }
     
     func handleReopen() {

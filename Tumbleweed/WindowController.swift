e2f49ff1b3768769b06990e6790a818c1645830b
Chloe Stars <hachidorii@icloud.com>
2015-07-26 13:11:24 -0700

    Fancy titlebar look using INAppStoreWindow.

--- Tumbleweed/WindowController.swift
+++ Tumbleweed/WindowController.swift
@@ -7,10 +7,21 @@
 //
 
 import Cocoa
+import INAppStoreWindow
 
 class WindowController : NSWindowController {
+    @IBOutlet var appWindow : INAppStoreWindow!
+    @IBOutlet var titleBarView : NSView!
     override func windowWillLoad() {
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "handleReopen", name: "handleReopen", object: nil)
+        
+        //        let appWindow = self.window as! INAppStoreWindow
+        appWindow.trafficLightButtonsLeftMargin = 14.0
+        appWindow.titleBarHeight = 33.0
+        
+        self.titleBarView.frame = appWindow.titleBarView.bounds
+        self.titleBarView.autoresizingMask  = [NSAutoresizingMaskOptions.ViewWidthSizable, NSAutoresizingMaskOptions.ViewHeightSizable]
+        appWindow.titleBarView.addSubview(self.titleBarView)
     }
     
     func handleReopen() {
